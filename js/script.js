a7c0c03a30885edd8089746f664538a359638afa
UliVazquez <uliv.contacto@gmail.com>
2025-01-27 15:33:22 -0300

    added cv and portofolio projects

--- js/script.js
+++ js/script.js
@@ -1,6 +1,6 @@
 const toggleTheme = document.getElementById("toggle-theme");
 const toggleIcon = document.getElementById("toggle-icon");
-const toogleText = document.getElementById("toggle-text");
+const toggleText = document.getElementById("toggle-text");
 
 const toggleColors = document.getElementById("toggle-colors");
 
@@ -10,13 +10,15 @@ toggleTheme.addEventListener("click", () => {
     document.body.classList.toggle("dark");
     if (toggleIcon.src.includes("sun.svg")) {
         toggleIcon.src = "assets/icons/moon.svg";
-        toogleText.textContent = "Dark mode";
+        toggleText.textContent = "Dark mode";
     } else {
         toggleIcon.src = "assets/icons/sun.svg";
-        toogleText.textContent = "Light mode";
+        toggleText.textContent = "Light mode";
     }
 });
 
+
+
 toggleColors.addEventListener("click", (e) => {
-    if (e.target.dataset.color) {rootStyles.setProperty("--primary-color", e.target.dataset.color);};
+    if (e.target.dataset.color) { rootStyles.setProperty("--primary-color", e.target.dataset.color); };
 });
